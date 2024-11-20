7b0139b975b0bc3fcb053aac5a8ff3c72c29a935
Dake Peng <pengd@carleton.edu>
2024-11-20 00:03:21 -0600

    updated idea

---
+++ basic_SVM.py
@@ -0,0 +1,6 @@
+# idea:
+# get word vector -> weight with TF-IDF to get document vectors
+# remove the category "BOTH"
+# use support vector machines to classify, 2 layer structure
+# 1. does the document contain a story? Y -> 2; N -> NONE
+# 2. is the story an election story? -> Y -> ELECTION; N - > NONELECTION
